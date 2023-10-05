5be13ebcad04d941cc9f5f77a1a16ff986d8010c
LeticiaCassol <106815953+LeticiaCassol@users.noreply.github.com>
2023-10-05 10:57:14 -0300

    Update main.js

--- main.js
+++ main.js
@@ -10,3 +10,13 @@ function tocaSom (seletorAudio) {
     }
     
 }
+    
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = '#som_${instrumento}'; //template string
+}
