22691e633abaf324c113cbd7589af1d915665e2d
LeticiaCassol <106815953+LeticiaCassol@users.noreply.github.com>
2023-10-19 11:05:43 -0300

    Update main.js

--- main.js
+++ main.js
@@ -19,4 +19,21 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     const idAudio = '#som_${instrumento}'; //template string
+
+tecla.onclick = function () {
+    tocaSom(idAudio);
+}
+
+tecla.onkeydown = function (evento) {
+
+    if (evento.code ==='Space'|| evento.code === 'Enter') {
+        tecla.classList.add('ativa');
+    }
+    
+}
+
+tecla.onkeyup = function () {
+    tecla.classList.remove('ativa');
+}
+
 }
